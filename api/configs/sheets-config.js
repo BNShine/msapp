f8e65daf113c0f3405689628b5933af7b6da9985
BNShine <alanqms2@gmail.com>
2025-10-01 01:44:55 -0300

    Add files via upload

---
+++ api/configs/sheets-config.js
@@ -0,0 +1,8 @@
+// api/configs/sheets-config.js
+
+export const SHEET_NAME_APPOINTMENTS = 'Datatest';
+export const SHEET_NAME_EMPLOYEES = 'Employees';
+export const SHEET_NAME_FRANCHISES = 'Regions';
+export const SHEET_NAME_USERS = 'Users';
+export const SHEET_NAME_ROLES = 'Roles';
+export const SHEET_NAME_TECHNICIANS = 'Technicians';
