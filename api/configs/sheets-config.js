075d99f4ba4cb9e1a0cfecb5edc54521754fad34
BNShine <alanqms2@gmail.com>
2025-10-02 19:33:54 -0300

    Update sheets-config.js

--- api/configs/sheets-config.js
+++ api/configs/sheets-config.js
@@ -6,3 +6,4 @@ export const SHEET_NAME_FRANCHISES = 'Regions';
 export const SHEET_NAME_USERS = 'Users';
 export const SHEET_NAME_ROLES = 'Roles';
 export const SHEET_NAME_TECHNICIANS = 'Technicians';
+export const SHEET_NAME_TECH_COVERAGE = 'TechCoverageData';
