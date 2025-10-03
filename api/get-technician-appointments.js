7631123e07ae92f1a0f290b3d096f35d89fc6b4f
BNShine <alanqms2@gmail.com>
2025-10-03 02:07:53 -0300

    Update get-technician-appointments.js

--- api/get-technician-appointments.js
+++ api/get-technician-appointments.js
@@ -61,6 +61,11 @@ export default async function handler(req, res) {
                 code: getCellValue('Code'),
                 verification: getCellValue('Verification') || 'Scheduled', 
                 petShowed: getCellValue('Pet Showed'),
+                serviceValue: getCellValue('Service Value'), // Adding this for reference
+                serviceShowed: getCellValue('Service Showed'), // NEW: For inline edit/save calc
+                tips: getCellValue('Tips'), // NEW: For inline edit/save calc
+                percentage: getCellValue('Percentage'), // NEW: For inline edit/save calc
+                paymentMethod: getCellValue('Method'), // NEW: For inline edit/save calc
             };
         }).filter(a => a !== null);
 
